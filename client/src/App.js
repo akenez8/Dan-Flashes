3bc15ddcccac7191b20f1ef6d4efc0a7051e6397
peter <+>
2022-11-28 15:22:20 -0500

    added router, navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
-import React, { useState, useEffect } from 'react';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import NavBar from "./NavBar"
 
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <header className="App-header">
         <button className="logoutButton" >Logout</button>
       <NavBar />
       </header>
       <h1 className="siteHeader">Dan Flashes</h1>
+      </Router>
     </div>
   );
 }
