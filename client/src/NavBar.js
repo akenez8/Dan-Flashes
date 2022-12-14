6c2992c3d68330f6c78ce468dd59e6f1d7cea976
peter <+>
2022-12-14 18:19:30 -0500

    navButton css, homeimg ternary

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -24,7 +24,7 @@ return (
         <NavLink exact to="/" style={{ marginRight: "10px" }}><button className="navButton">Home</button></NavLink>
         <NavLink exact to="/Shop" style={{ marginRight: "10px" }}><button className="navButton">Shop</button></NavLink>
         <NavLink exact to="MyShirts" style={{ marginRight: "10px" }}><button className="navButton">My Shirts</button></NavLink>
-        <NavLink exact to="/Designs" style={{ marginRight: "10px" }}><button className="navButton" Designs></button></NavLink>
+        <NavLink exact to="/Designs" style={{ marginRight: "10px" }}><button className="navButton"> Designs</button></NavLink>
         {currentUser ? null : <NavLink exact to="/Login" style={{ marginRight: "10px" }}><button className="navButton">Login</button></NavLink>}
         {currentUser ? null : <NavLink exact to="Signup" style={{ marginRight: "10px" }}><button className="navButton">Sign Up</button></NavLink>}
         {currentUser ? <NavLink exact to="/UpdateUser" style={{ marginRight: "10px" }}><button className="navButton">Change Username</button></NavLink> : null}
